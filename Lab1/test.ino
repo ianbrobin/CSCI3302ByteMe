bd648e9fb74d49fefa85e727760f1bf8509b26d8
Ian Brobin <iabr1572@colorado.edu>
2020-01-22 14:37:34 -0700

    Test code for lab1

---
+++ Lab1/test.ino
@@ -0,0 +1,12 @@
+#include <Sparki.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  sparki.ping();
+  sparki.print("Hello World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
