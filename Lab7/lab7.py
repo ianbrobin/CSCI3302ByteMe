dae7d825f9f9a6f7d30a86a5d4e5cfaa861b7ed3
ChandlerGarthwaite <chandler.garthwaite@gmail.com>
2020-04-07 16:39:37 -0700

    orientation

--- Lab7/lab7.py
+++ Lab7/lab7.py
@@ -10,11 +10,12 @@ if __name__ == "__main__":
     parser.add_argument('-theta', type=float, nargs=1, default=1.0, help="Goal theta location")
     args = parser.parse_args()
     rospy.init_node("goalPublisher")
+
     publisher_goal = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
     goalpose = PoseStamped()
     goalpose.pose.position.x = args.x
     goalpose.pose.position.y = args.y
-    # goalpose.pose.position.z = args.theta
+    goalpose.pose.orientation.w = args.theta
     goalpose.pose.position.z = 0
     publisher_goal.publish(goalpose)
     print(args.x,args.y,args.theta)
