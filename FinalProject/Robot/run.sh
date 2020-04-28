53bdb488ec047f7b74309a892ac5a409e82af900
aual1780 <aual1780@colorado.edu>
2020-04-28 02:12:42 -0600

    udpate run file

--- FinalProject/Robot/run.sh
+++ FinalProject/Robot/run.sh
@@ -2,4 +2,5 @@ x-terminal-emulator -e roscore
 x-terminal-emulator -e roslaunch rosbridge_server rosbridge_websocket.launch
 x-terminal-emulator -e rosrun turtlesim TicTacTurtle
 x-terminal-emulator -e python3 TicTacSolver.py
+x-terminal-emulator -e python3 TicTacDriver.py
 python3 TicTacCore.py
